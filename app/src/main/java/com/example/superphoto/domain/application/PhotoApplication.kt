ece8085eb3d092ebbd395ec35fc273a9c7814957
ngtiendat90 <you@example.com>
2025-09-16 10:21:57 +0700

    update

---
+++ app/src/main/java/com/example/superphoto/domain/application/PhotoApplication.kt
@@ -0,0 +1,22 @@
+package com.example.superphoto.domain.application
+
+import android.app.Application
+import com.example.superphoto.di.module
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class PhotoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@PhotoApplication)
+            modules(module)
+        }
+    }
+
+}
